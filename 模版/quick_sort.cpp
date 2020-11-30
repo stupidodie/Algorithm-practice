a32396590fc251f1c66f2ba0cfed57d657067ad7
guanran tai <taiguanran@gmail.com>
2020-11-30 16:59:33 +0800

    Update quick_sort.cpp

--- 模版/quick_sort.cpp
+++ 模版/quick_sort.cpp
@@ -6,7 +6,8 @@ int q[100010];
 
 void quick_sort(int q[],int l ,int r){
     if(l>=r) return ;
-    int x=q[(l+r)>>1];int i=l-1;int j=r+1;//选l不能用i，r不能用j
+    int x=q[(l+r)>>1];int i=l-1;int j=r+1;
+    //选l不能用i，r不能用j
     while(i<j){
         while(q[++i]<x);
         while(q[--j]>x);
@@ -16,7 +17,9 @@ void quick_sort(int q[],int l ,int r){
             q[j]=temp;
         }
     }
-    quick_sort(q,l,j);quick_sort(q,j+1,r);//选j的话,j和j+1;选i的话 i-1和i；因为停下来的时候i是>=x,j是<=x,i右半边，j左半边
+    //选j的话,j和j+1;选i的话 i-1和i；因为停下来的时候i是>=x,j是<=x,i右半边，j左半边
+    quick_sort(q,l,j);quick_sort(q,j+1,r);
+    
 }
 int main(){
     int n;
